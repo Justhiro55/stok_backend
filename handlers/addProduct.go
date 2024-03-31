e14317d8d510a9f7c9a99938ee076dfdfc966a82
JustHiro55 <hagi.hiro9829@gmail.com>
2024-03-31 13:51:06 +0900

    feat: Add functionality to insert received request data into db

--- handlers/addProduct.go
+++ handlers/addProduct.go
@@ -1,16 +1,28 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"main.go/db"
 )
 
+type ProductData struct {
+	ProductName string `json:"product_name"`
+	BrandName   string `json:"brand_name"`
+	ImagePath   string `json:"image_path"`
+}
+
 func AddProductHandler(w http.ResponseWriter, r *http.Request) {
-	productName := "test_productName1"
-	brandName := "test_brandName1"
-	imagePath := "test_imagePath1"
+	var data ProductData
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		log.Println("Error decoding JSON:", err)
+		http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
+		return
+	}
 
 	db.Init()
 
@@ -21,7 +33,7 @@ func AddProductHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = tx.Exec("INSERT INTO brands (name) VALUES ($1)", brandName)
+	_, err = tx.Exec("INSERT INTO brands (name) VALUES ($1)", data.BrandName)
 	if err != nil {
 		tx.Rollback()
 		log.Println("Error inserting brand:", err)
@@ -30,7 +42,7 @@ func AddProductHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var brandID int64
-	err = tx.QueryRow("SELECT brand_id FROM brands WHERE name = $1", brandName).Scan(&brandID)
+	err = tx.QueryRow("SELECT brand_id FROM brands WHERE name = $1", data.BrandName).Scan(&brandID)
 	if err != nil {
 		tx.Rollback()
 		log.Println("Error getting brand ID:", err)
@@ -38,7 +50,7 @@ func AddProductHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = tx.Exec("INSERT INTO products (name, brand_id) VALUES ($1, $2)", productName, brandID)
+	_, err = tx.Exec("INSERT INTO products (name, brand_id) VALUES ($1, $2)", data.ProductName, brandID)
 	if err != nil {
 		tx.Rollback()
 		log.Println("Error inserting product:", err)
@@ -47,7 +59,7 @@ func AddProductHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var productID int64
-	err = tx.QueryRow("SELECT product_id FROM products WHERE name = $1", productName).Scan(&productID)
+	err = tx.QueryRow("SELECT product_id FROM products WHERE name = $1", data.ProductName).Scan(&productID)
 	if err != nil {
 		tx.Rollback()
 		log.Println("Error getting product ID:", err)
@@ -55,7 +67,7 @@ func AddProductHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = tx.Exec("INSERT INTO images (image_path, product_id) VALUES ($1, $2)", imagePath, productID)
+	_, err = tx.Exec("INSERT INTO images (image_path, product_id) VALUES ($1, $2)", data.ImagePath, productID)
 	if err != nil {
 		tx.Rollback()
 		log.Println("Error inserting image:", err)
